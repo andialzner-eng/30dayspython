026c8ef3802d797a532d2d228b1355b8df794fcb
andialzner-eng <andialzner@web.de>
2026-04-28 20:24:19 +0200

    Day 2 fertig

---
+++ 02_Day_Variables_builtin_functions/variables_eigen.py
@@ -0,0 +1,81 @@
+print("Day 2: 30 Days of python programming")
+
+# Variables
+first_name = "Andi"
+last_name = "Alzner"
+full_name = first_name + " " + last_name
+country = "Germany"
+city = "Munich"
+age = 34
+year = 2026
+is_married = True
+is_true = True
+is_light_on = False 
+street, number, postal_code = "Reiherweg", "18c", "81827"
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Full name:", full_name)
+print("Country:", country)
+print("City:", city)
+print("Age:", age)
+print("Year:", year)
+print("Married:", is_married)
+print("Is true:", is_true)
+print("Is light on:", is_light_on)
+print("Street:", street)
+print("Number:", number)
+print("Postal code:", postal_code)
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(street))
+print(type(number))
+print(type(postal_code))
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+modulo = num_one % num_two
+floor_division = num_one // num_two
+
+print("Total:", total) 
+print("Difference:", diff)
+print("Product:", product)
+print("Division:", division)
+print("Modulo:", modulo)
+print("Floor division:", floor_division)
+
+area_of_circle = 3.14 * (30 ** 2)
+print("Area of a circle with radius 30:", area_of_circle)
+
+circum_of_circle = 2 * 3.14 * 30
+print("Circumference of a circle with radius 30:", circum_of_circle)
+
+radius = int(input("Enter the radius of a circle: "))
+area_of_circle = 3.14 * (radius ** 2)
+print("Area of a circle with radius", radius, "is:", area_of_circle)
+
+first_name = input("Enter your name: ")
+print("Your name is:", first_name)
+last_name = input("Enter your last name: ")
+print("Your last name is:", last_name)
+country = input("Enter your country: ")
+age = input("Enter your age: ")
+print("Your age is:", age)
+
